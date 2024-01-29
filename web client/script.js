9627dc3d538c8258829eb9a8abb3b83bad240d56
dosmakcic <73404686+dosmakcic@users.noreply.github.com>
2024-01-29 13:21:11 +0100

    Update script.js

--- web client/script.js
+++ web client/script.js
@@ -1,45 +1,80 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const feedButton = document.getElementById('feedButton');
-    const feedAmount = document.getElementById('feedAmount');
-    const logEntries = document.getElementById('logEntries');
-    const connectionStatus = document.getElementById('connectionStatus');
-    const defaultAmountInput = document.getElementById('defaultAmount');
-    const setDefaultButton = document.getElementById('setDefaultButton');
-  
-    const ws = new WebSocket('ws://192.168.X.XXX:8888/ws');
-  
-    ws.onopen = function () {
-      connectionStatus.textContent = 'Connected';
-    };
-  
-    ws.onclose = function () {
-      connectionStatus.textContent = 'Disconnected';
-    };
-  
-    ws.onmessage = function (event) {
+  const dogFeedButton = document.getElementById('feedDogButton');
+  const dogFeedAmount = document.getElementById('dogFeedAmount');
+  const catFeedButton = document.getElementById('feedCatButton');
+  const catFeedAmount = document.getElementById('catFeedAmount');
+  const logEntries = document.getElementById('logEntries');
+  const connectionStatus = document.getElementById('connectionStatus');
+  const defaultAmountInput = document.getElementById('defaultAmount');
+  const setDefaultButton = document.getElementById('setDefaultButton');
+  const dogBowlStatusSpan = document.getElementById('dogBowlStatus');
+  const catBowlStatusSpan = document.getElementById('catBowlStatus');
+
+  const ws = new WebSocket('ws://192.168.X.XXX:8888/ws');
+  const refreshInterval = 5000; // Postavite interval prema potrebi, npr. svakih 5000 milisekundi (5 sekundi)
+
+  function checkStatusAutomatically() {
+    ws.send(JSON.stringify({ action: 'checkStatus' }));
+  }
+
+  // Pozovi funkciju checkStatusAutomatically svakih refreshInterval milisekundi
+  const refreshIntervalId = setInterval(checkStatusAutomatically, refreshInterval);
+
+  // Event listener za zatvaranje stranice, kako bismo prekinuli interval kada korisnik napusti stranicu
+  window.addEventListener('beforeunload', function () {
+    clearInterval(refreshIntervalId);
+  });
+
+  ws.onopen = function () {
+    connectionStatus.textContent = 'Connected';
+  };
+
+  ws.onclose = function () {
+    connectionStatus.textContent = 'Disconnected';
+  };
+
+  ws.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+
+    if (data.action === 'statusUpdate') {
+      dogBowlStatusSpan.textContent = `Level: ${data.dogBowlStatus} g`;
+      catBowlStatusSpan.textContent = `Level: ${data.catBowlStatus} g`;
+    } else {
       const message = document.createElement('div');
       message.textContent = event.data;
       logEntries.appendChild(message);
-    };
+    }
+  };
 
-    setDefaultButton.addEventListener('click', function () {
-      const defaultAmount = defaultAmountInput.value;
-      if (defaultAmount) {
-        ws.send(JSON.stringify({ action: 'setDefault', amount: defaultAmount }));
-        defaultAmountInput.value = '';
-      } else {
-        alert('Please enter the default amount of food.');
-      }
-    });
-  
-    feedButton.addEventListener('click', function () {
-      const amount = feedAmount.value;
-      if (amount) {
-        ws.send(JSON.stringify({ action: 'feed', amount }));
-        feedAmount.value = '';
-      } else {
-        alert('Please enter the amount of food.');
-      }
-    });
+  setDefaultButton.addEventListener('click', function () {
+    const defaultAmount = defaultAmountInput.value;
+    if (defaultAmount) {
+      ws.send(JSON.stringify({ action: 'setDefault', amount: defaultAmount }));
+      defaultAmountInput.value = '';
+    } else {
+      alert('Please enter the default amount of food.');
+    }
   });
+
+  dogFeedButton.addEventListener('click', function () {
+    const amount = dogFeedAmount.value;
+    if (amount) {
+      ws.send(JSON.stringify({ action: 'feedDog', amount }));
+      dogFeedAmount.value = '';
+    } else {
+      alert('Please enter the amount of dog food.');
+    }
+  });
+
+  catFeedButton.addEventListener('click', function () {
+    const amount = catFeedAmount.value;
+    if (amount) {
+      ws.send(JSON.stringify({ action: 'feedCat', amount }));
+      catFeedAmount.value = '';
+    } else {
+      alert('Please enter the amount of cat food.');
+    }
+  });
+});
+
   
