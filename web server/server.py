1c0eac55903501b738d09af93882a9923b64cfe5
Antonio Omazić <73439988+aomazic@users.noreply.github.com>
2024-01-30 10:09:32 +0100

    Update server.py

--- web server/server.py
+++ web server/server.py
@@ -1,4 +1,4 @@
-                                                                                               duopet_server.py
+import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
@@ -11,7 +11,8 @@ db = mdb.connect(host="localhost", user="user", password="password", database="d
 cursor = db.cursor()
 
 mqtt_broker = "192.168.23.67"
-mqtt_topic = "duoPet/status"
+mqtt_topic_detections = "duoPet/detections"
+mqtt_topic_status= "duoPet/status"
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -29,12 +30,23 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
 def on_message(client, userdata, msg):
     data = msg.payload.decode('utf-8')
-    save_to_database(data)
+    
+    # Check the MQTT topic and handle messages accordingly
+    if msg.topic == mqtt_topic_detections:
+        is_bowl_full, pet_type = data.split(',')
+        save_to_database(is_bowl_full, pet_type)
+    elif msg.topic == mqtt_topic_status:
+        numbers = data.split(',')
+        for client in wss:
+            client.write_message(numbers)
+      
+    else:
+        print(f"Unknown topic: {msg.topic}")
 
-def save_to_database(data):
+def save_to_database(is_bowl_full, pet_type):
     try:
         sql = "INSERT INTO petDetections (detection_time, pet_type, is_bowl_full) VALUES (NOW(), %s, %s)"
-        values = ("your_pet_type", data)
+        values = (pet_type, is_bowl_full)
         cursor.execute(sql, values)
         db.commit()
         print("Data saved to database.")
@@ -45,7 +57,8 @@ def save_to_database(data):
 client = mqtt.Client()
 client.on_message = on_message
 client.connect(mqtt_broker, 1883, 60)
-client.subscribe(mqtt_topic)
+client.subscribe(mqtt_topic_detections)
+client.subscribe(mqtt_topic_status)
 client.loop_start()
 
 application = tornado.web.Application([(r'/ws', WSHandler),])
